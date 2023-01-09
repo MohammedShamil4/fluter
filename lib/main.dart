c611f6183a074460fe68c108f9dfb0ee26251c3b
MohammedShamil4 <mohammedshamilsha34@gmail.com>
2023-01-09 14:09:13 +0530

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: Text(
+      " Hello welcome To Flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.cyan, fontSize: 30),
+    ),
+  ));
+}
